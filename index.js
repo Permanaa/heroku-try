216b69e6e287623853e20764864e4016d7fa6dcb
Lulu Irman Permana <irmanpermana.lu@gmail.com>
2019-09-05 05:58:39 +0700

    fix index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http')
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'application/json'}),
-    response.write('{"message": "Hello World!"}');
-    response.end();
+    res.write('{"message": "Hello World!"}');
+    res.end();
 }).listen(process.env.PORT || 8000)
 
 console.log('Server running on port 8000');
